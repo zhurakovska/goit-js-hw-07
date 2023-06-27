1c5db61afd749044244d272cd1e27a839d116d57
zhurakovska <n.sinawka@gmail.com>
2023-06-27 18:56:05 +0300

    done task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector('.gallery')
+
+const imgItem =({preview, original, description} = {}) =>(
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-original-url = "${original}"
+            alt="${description}" 
+            />
+        </a>
+    </li>`
+    );
+
+imgItem(galleryItems)
+
+const galleryImgArr = galleryItems.map((el) => imgItem(el))   
+listEl.insertAdjacentHTML('beforeend', galleryImgArr.join(''));
+
+
+const onImgClick = (event) =>{
+    event.preventDefault()
+    const {target: {nodeName, dataset}} = event
+
+    if (nodeName !== 'IMG') {
+        return;
+    }
+
+    const {originalUrl} = dataset;
+    const makeImgBig = basicLightbox.create(`
+    <img src="${originalUrl}" width="800" height="600">
+`);
+    makeImgBig.show()
+}
+
+listEl.addEventListener('click', onImgClick)
